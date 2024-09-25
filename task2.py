5fc12864825c0e365dba07e7a34c84bca22e7fea
theyuliee <yuliiaoksen@gmail.com>
2024-09-25 10:59:23 +0300

    Update task2.py
    updated homework

--- task2.py
+++ task2.py
@@ -1,11 +1,14 @@
 import random
-def get_numbers_ticket(min, max, quantity) :
-    if min >= 1 and max <= 1000 :
+def get_numbers_ticket(min: int, max: int, quantity: int) :
+    if min >= 1 and max <= 1000 and min < max and (max - min) >= quantity:
        lottery_numbers = {}
        lottery_numbers = random.sample(range(min, max + 1), quantity)
-       return number_ticket
+       lottery_numbers.sort(key = None, reverse = False )
+       return lottery_numbers
     else :
         print ('Введіть значення від 1 до 1000')
+        return []
 
 
 
+print (get_numbers_ticket(1, 10, 2))
